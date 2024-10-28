465448d8103780dfa1f5880896db42096569108d
Frank Yang <frankyang233@gmail.com>
2024-10-27 19:13:05 -0700

    lava shader wip

---
+++ Assets/Scripts/CustomShape.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomShape : MonoBehaviour
+{
+    public enum Shape
+    {
+        Sphere,
+        Cube,
+        Torus,
+        Cone,
+    }
+}
