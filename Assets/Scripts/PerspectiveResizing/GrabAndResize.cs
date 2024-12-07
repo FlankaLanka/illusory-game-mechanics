576a3294b78ba0dce520f306d01328050d46c265
Frank Yang <frankyang233@gmail.com>
2024-12-07 02:22:21 -0800

    perspective resizing mechanic mostly working

--- Assets/Scripts/PerspectiveResizing/GrabAndResize.cs
+++ Assets/Scripts/PerspectiveResizing/GrabAndResize.cs
@@ -7,10 +7,12 @@ public class GrabAndResize : MonoBehaviour
     public Transform playerCam;
     public Transform grabbedObject;
 
-    public float maxDistanceAway = 100f;
+    public float maxDistanceAway = 10f;
 
     private Vector3 objectRelativePosition;
     private Quaternion objectRelativeRotation;
+    private Vector3 objectStartingScale;
+
 
     void Update()
     {
@@ -18,7 +20,7 @@ public class GrabAndResize : MonoBehaviour
         {
             TryGrab();
         }
-        else if (Input.GetMouseButtonUp(0))
+        else if (Input.GetMouseButtonDown(1))
         {
             TryRelease();
         }
@@ -39,12 +41,17 @@ public class GrabAndResize : MonoBehaviour
         {
             if (hit.transform.GetComponent<Grabbable>())
             {
-                hit.transform.GetComponent<Rigidbody>().isKinematic = true;
+                Rigidbody hit_rb = hit.transform.GetComponent<Rigidbody>();
+                hit_rb.isKinematic = true;
 
                 grabbedObject = hit.transform;
 
                 objectRelativePosition = playerCam.InverseTransformPoint(grabbedObject.position);
                 objectRelativeRotation = Quaternion.Inverse(playerCam.rotation) * grabbedObject.rotation;
+                objectStartingScale = grabbedObject.lossyScale;
+
+                originalScaleFromCamera = grabbedObject.lossyScale;
+                originalDistFromCamera = (grabbedObject.position - playerCam.position).magnitude;
 
                 Debug.Log("Object grabbed");
             }
@@ -58,11 +65,20 @@ public class GrabAndResize : MonoBehaviour
 
         Debug.Log("Try releasing");
 
-        grabbedObject.GetComponent<Rigidbody>().isKinematic = false;
+        Rigidbody hit_rb = grabbedObject.GetComponent<Rigidbody>();
+        hit_rb.isKinematic = false;
+
         grabbedObject = null;
 
         Debug.Log("Object released");
     }
+
+
+    public float resizeFactor = 0f;
+    private Vector3 originalScaleFromCamera;
+    private float originalDistFromCamera;
+
+    private float distAwayFromWall;
 
     private void TryHold()
     {
@@ -75,6 +91,40 @@ public class GrabAndResize : MonoBehaviour
         grabbedObject.position = playerCam.TransformPoint(objectRelativePosition);
         grabbedObject.rotation = playerCam.rotation * objectRelativeRotation;
 
-        //perspective grab next
+        //calculate resizeFactor, make object reach closest wall
+        Vector3 curDirection = (grabbedObject.position - playerCam.position).normalized;
+
+        Ray ray = new Ray(playerCam.position, curDirection);
+        RaycastHit[] hits = Physics.RaycastAll(ray);
+        distAwayFromWall = maxDistanceAway;
+
+        for(int i = 0; i < hits.Length; i++)
+        {
+            if (hits[i].transform.GetComponent<Grabbable>())
+                continue;
+
+            distAwayFromWall = Mathf.Min(distAwayFromWall, hits[i].distance);
+            //technically can break here since we looking for smallest only
+            //break;
+        }
+
+        //distAwayFromWall = Vector3.Distance(GetReasonableFinalDistance(grabbedObject, playerCam.position, playerCam.position + distAwayFromWall * curDirection, 0.05f), playerCam.position);
+        resizeFactor = distAwayFromWall / originalDistFromCamera;
+
+        grabbedObject.position = (curDirection * originalDistFromCamera) * resizeFactor + playerCam.position;
+        grabbedObject.localScale = originalScaleFromCamera * resizeFactor;
+    }
+
+    //TODO: make sure the box isn't colliding into wall
+    private Vector3 GetReasonableFinalDistance(Transform t, Vector3 start, Vector3 end, float intervals)
+    {
+        float lerp = 1f;
+        Vector3 center = Vector3.Lerp(start, end, lerp);
+        while(Physics.OverlapBox(center,t.GetComponent<BoxCollider>().size, t.rotation).Length > 0 && lerp >= intervals)
+        {
+            lerp -= intervals;
+        }
+
+        return Vector3.Lerp(start, end, lerp);
     }
 }
