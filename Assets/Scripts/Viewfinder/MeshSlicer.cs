b5f79034b6b8dbdc917f61b127e5fa49e7f68dd1
Frank Yang <frankyang233@gmail.com>
2024-12-09 11:12:47 -0800

    buggy implementation of fix hollow part of cut

--- Assets/Scripts/Viewfinder/MeshSlicer.cs
+++ Assets/Scripts/Viewfinder/MeshSlicer.cs
@@ -21,6 +21,8 @@ public class MeshSlicer
         public Vector3 normal;
         public Vector2 uv;
 
+        public CustomVertexDataStruct() { }
+
         public CustomVertexDataStruct(Vector3 v, Vector3 n, Vector2 u)
         {
             vertex = v;
@@ -65,6 +67,13 @@ public class MeshSlicer
         CustomMeshDataStruct customLeftMesh = new();
         CustomMeshDataStruct customRightMesh = new();
 
+        //used to check if vertex already in array
+        Dictionary<(Vector3, Vector3, Vector2), int> vertexDataMap = new();
+
+        //used for fixing the hollowness after cutting
+        List<int> generatedVerticesLeft = new();
+        List<int> generatedVerticesRight = new();
+
         for (int i = 0; i < mainMesh.triangles.Length; i += 3)
         {
             int v1 = mainMesh.triangles[i];
@@ -86,7 +95,7 @@ public class MeshSlicer
                 CustomVertexDataStruct datav1 = new(mainMesh.vertices[v1], mainMesh.normals[v1], mainMesh.uv[v1]);
                 CustomVertexDataStruct datav2 = new(mainMesh.vertices[v2], mainMesh.normals[v2], mainMesh.uv[v2]);
                 CustomVertexDataStruct datav3 = new(mainMesh.vertices[v3], mainMesh.normals[v3], mainMesh.uv[v3]);
-                AddTriangleToMesh(datav1, datav2, datav3, customLeftMesh);
+                AddTriangleToMesh(datav1, datav2, datav3, customLeftMesh, vertexDataMap);
                 //Debug.Log("HERE IN LEFT");
             }
             else if (!v1NormalSide && !v2NormalSide && !v3NormalSide)
@@ -94,38 +103,46 @@ public class MeshSlicer
                 CustomVertexDataStruct datav1 = new(mainMesh.vertices[v1], mainMesh.normals[v1], mainMesh.uv[v1]);
                 CustomVertexDataStruct datav2 = new(mainMesh.vertices[v2], mainMesh.normals[v2], mainMesh.uv[v2]);
                 CustomVertexDataStruct datav3 = new(mainMesh.vertices[v3], mainMesh.normals[v3], mainMesh.uv[v3]);
-                AddTriangleToMesh(datav1, datav2, datav3, customRightMesh);
+                AddTriangleToMesh(datav1, datav2, datav3, customRightMesh, vertexDataMap);
                 //Debug.Log("HERE IN RIGHT");
             }
             else
             {
                 if (v1NormalSide && !v2NormalSide && !v3NormalSide)
                 {
-                    Patch((v2, v3), v1, customRightMesh, customLeftMesh, planeNormal, planePoint, mainMesh, meshTransform);
+                    Patch((v2, v3), v1, customRightMesh, customLeftMesh, planeNormal, planePoint, mainMesh, meshTransform, vertexDataMap, generatedVerticesRight, generatedVerticesLeft);
                 }
                 else if (v1NormalSide && v2NormalSide && !v3NormalSide)
                 {
-                    Patch((v1, v2), v3, customLeftMesh, customRightMesh, planeNormal, planePoint, mainMesh, meshTransform);
+                    Patch((v1, v2), v3, customLeftMesh, customRightMesh, planeNormal, planePoint, mainMesh, meshTransform, vertexDataMap, generatedVerticesLeft, generatedVerticesRight);
                 }
                 else if (v1NormalSide && !v2NormalSide && v3NormalSide)
                 {
-                    Patch((v3, v1), v2, customLeftMesh, customRightMesh, planeNormal, planePoint, mainMesh, meshTransform);
+                    Patch((v3, v1), v2, customLeftMesh, customRightMesh, planeNormal, planePoint, mainMesh, meshTransform, vertexDataMap, generatedVerticesLeft, generatedVerticesRight);
                 }
                 else if (!v1NormalSide && v2NormalSide && v3NormalSide)
                 {
-                    Patch((v2, v3), v1, customLeftMesh, customRightMesh, planeNormal, planePoint, mainMesh, meshTransform);
+                    Patch((v2, v3), v1, customLeftMesh, customRightMesh, planeNormal, planePoint, mainMesh, meshTransform, vertexDataMap, generatedVerticesLeft, generatedVerticesRight);
                 }
                 else if (!v1NormalSide && v2NormalSide && !v3NormalSide)
                 {
-                    Patch((v3, v1), v2, customRightMesh, customLeftMesh, planeNormal, planePoint, mainMesh, meshTransform);
+                    Patch((v3, v1), v2, customRightMesh, customLeftMesh, planeNormal, planePoint, mainMesh, meshTransform, vertexDataMap, generatedVerticesRight, generatedVerticesLeft);
                 }
                 else if (!v1NormalSide && !v2NormalSide && v3NormalSide)
                 {
-                    Patch((v1, v2), v3, customRightMesh, customLeftMesh, planeNormal, planePoint, mainMesh, meshTransform);
+                    Patch((v1, v2), v3, customRightMesh, customLeftMesh, planeNormal, planePoint, mainMesh, meshTransform, vertexDataMap, generatedVerticesRight, generatedVerticesLeft);
                 }
                 //Debug.Log("HERE IN SLICE");
             }
         }
+
+        //patched plane here
+        AddCenterPoint(customLeftMesh, generatedVerticesLeft);
+        PatchPlane(customLeftMesh, generatedVerticesLeft);
+
+        AddCenterPoint(customRightMesh, generatedVerticesRight);
+        PatchPlane(customRightMesh, generatedVerticesRight);
+
 
         leftMesh.vertices = customLeftMesh.verticesList.ToArray();
         rightMesh.vertices = customRightMesh.verticesList.ToArray();
@@ -140,6 +157,39 @@ public class MeshSlicer
         rightMesh.triangles = customRightMesh.trianglesList.ToArray();
 
         //DebuggingLog(mainMesh, leftMesh, rightMesh);
+    }
+
+    private static void AddCenterPoint(CustomMeshDataStruct customMesh, List<int> generatedVertices)
+    {
+        CustomVertexDataStruct output = new();
+
+        for(int i = 0; i < generatedVertices.Count; i++)
+        {
+            output.vertex += customMesh.verticesList[generatedVertices[i]];
+            output.normal += customMesh.normalsList[generatedVertices[i]];
+            output.uv += customMesh.uvsList[generatedVertices[i]];
+        }
+
+        output.vertex /= generatedVertices.Count;
+        output.normal /= generatedVertices.Count;
+        output.uv /= generatedVertices.Count;
+
+        Debug.Log("CENTER POINT: " + output.vertex);
+
+        customMesh.verticesList.Add(output.vertex);
+        customMesh.normalsList.Add(output.normal);
+        customMesh.uvsList.Add(output.uv);
+    }
+
+    private static void PatchPlane(CustomMeshDataStruct customMesh, List<int> generatedVertices)
+    {
+        for(int i = 0; i < generatedVertices.Count - 1; i++)
+        {
+            customMesh.trianglesList.Add(generatedVertices[i]);
+            customMesh.trianglesList.Add(customMesh.verticesList.Count - 1);
+
+            customMesh.trianglesList.Add(generatedVertices[i + 1]);
+        }
     }
 
     //private static void DebuggingLog(Mesh original, Mesh left, Mesh right)
@@ -181,8 +231,45 @@ public class MeshSlicer
     //    }
     //}
 
-    private static void AddTriangleToMesh(CustomVertexDataStruct v1, CustomVertexDataStruct v2, CustomVertexDataStruct v3, CustomMeshDataStruct customMesh)
+    private static void AddTriangleToMesh(CustomVertexDataStruct v1, CustomVertexDataStruct v2, CustomVertexDataStruct v3, CustomMeshDataStruct customMesh,
+                                          Dictionary<(Vector3, Vector3, Vector2), int> vertexDataMap)
     {
+        //if(vertexDataMap.ContainsKey((v1.vertex,v1.normal,v1.uv)))
+        //{
+        //    customMesh.trianglesList.Add(vertexDataMap[(v1.vertex, v1.normal, v1.uv)]);
+        //}
+        //else
+        //{
+        //    AddVertexDataToMesh(v1, customMesh);
+        //    customMesh.trianglesList.Add(customMesh.verticesList.Count - 1);
+
+        //    vertexDataMap.Add((v1.vertex, v1.normal, v1.uv), customMesh.verticesList.Count - 1);
+        //}
+
+        //if (vertexDataMap.ContainsKey((v2.vertex, v2.normal, v2.uv)))
+        //{
+        //    customMesh.trianglesList.Add(vertexDataMap[(v2.vertex, v2.normal, v2.uv)]);
+        //}
+        //else
+        //{
+        //    AddVertexDataToMesh(v2, customMesh);
+        //    customMesh.trianglesList.Add(customMesh.verticesList.Count - 1);
+
+        //    vertexDataMap.Add((v2.vertex, v2.normal, v2.uv), customMesh.verticesList.Count - 1);
+        //}
+
+        //if (vertexDataMap.ContainsKey((v3.vertex, v3.normal, v3.uv)))
+        //{
+        //    customMesh.trianglesList.Add(vertexDataMap[(v3.vertex, v3.normal, v3.uv)]);
+        //}
+        //else
+        //{
+        //    AddVertexDataToMesh(v3, customMesh);
+        //    customMesh.trianglesList.Add(customMesh.verticesList.Count - 1);
+
+        //    vertexDataMap.Add((v3.vertex, v3.normal, v3.uv), customMesh.verticesList.Count - 1);
+        //}
+
         AddVertexDataToMesh(v1, customMesh);
         AddVertexDataToMesh(v2, customMesh);
         AddVertexDataToMesh(v3, customMesh);
@@ -200,7 +287,8 @@ public class MeshSlicer
     }
 
     private static void Patch((int,int) vIndexSameSide, int vIndexOppositeSide, CustomMeshDataStruct customMeshDuo, CustomMeshDataStruct customMeshSolo,
-                              Vector3 planeNormal, Vector3 planePoint, Mesh mainMesh, Transform meshTransform)
+                              Vector3 planeNormal, Vector3 planePoint, Mesh mainMesh, Transform meshTransform, Dictionary<(Vector3, Vector3, Vector2), int> vertexDataMap,
+                              List<int> generatedVerticesDuo, List<int> generatedVerticesSolo)
     {
 
         float lerpTime1 = FindLerpOnPlane(planeNormal, planePoint, meshTransform.TransformPoint(mainMesh.vertices[vIndexSameSide.Item1]), meshTransform.TransformPoint(mainMesh.vertices[vIndexOppositeSide]));
@@ -218,13 +306,20 @@ public class MeshSlicer
         CustomVertexDataStruct soloVertexData1 = new(mainMesh.vertices[vIndexOppositeSide], mainMesh.normals[vIndexOppositeSide], mainMesh.uv[vIndexOppositeSide]);
         CustomVertexDataStruct lerpedVertexData1 = new(lerpedVertex1, lerpedNormal1, lerpedUVs1);
         CustomVertexDataStruct lerpedVertexData2 = new(lerpedVertex2, lerpedNormal2, lerpedUVs2);
-        AddTriangleToMesh(soloVertexData1, lerpedVertexData1, lerpedVertexData2, customMeshSolo);
+        AddTriangleToMesh(soloVertexData1, lerpedVertexData1, lerpedVertexData2, customMeshSolo, vertexDataMap);
+
+        //add current index of vert as it will be a new triangle later
+        generatedVerticesSolo.Add(customMeshSolo.verticesList.Count - 1);
 
         //patch double
         CustomVertexDataStruct duoVertexData1 = new(mainMesh.vertices[vIndexSameSide.Item1], mainMesh.normals[vIndexSameSide.Item1], mainMesh.uv[vIndexSameSide.Item1]);
         CustomVertexDataStruct duoVertexData2 = new(mainMesh.vertices[vIndexSameSide.Item2], mainMesh.normals[vIndexSameSide.Item2], mainMesh.uv[vIndexSameSide.Item2]);
-        AddTriangleToMesh(duoVertexData1, duoVertexData2, lerpedVertexData1, customMeshDuo);
-        AddTriangleToMesh(lerpedVertexData1, duoVertexData2, lerpedVertexData2, customMeshDuo);
+        AddTriangleToMesh(duoVertexData1, duoVertexData2, lerpedVertexData1, customMeshDuo, vertexDataMap);
+        AddTriangleToMesh(lerpedVertexData1, duoVertexData2, lerpedVertexData2, customMeshDuo, vertexDataMap);
+
+        //add current index of vert as it will be a new triangle later
+        generatedVerticesDuo.Add(customMeshDuo.verticesList.Count - 2);
+        generatedVerticesDuo.Add(customMeshDuo.verticesList.Count - 1);
     }
 
 
